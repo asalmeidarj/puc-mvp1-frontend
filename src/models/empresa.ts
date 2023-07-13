2b22c416c878fc498d44d0391ac12d1f072ec352
asalmeidarj <asalmeidarj@gmail.com>
2023-07-13 19:47:23 -0300

    feat: add type empresa

---
+++ src/models/empresa.ts
@@ -0,0 +1,13 @@
+export type IEmpresa = {
+    id: number,
+    nome: string,
+    cnpj: string,
+    descricao: string,
+    logradouro: string,
+    numero: string,
+    complemento: string,
+    bairro: string,
+    cidade: string,
+    estado: string,
+    data_insercao: string
+}
