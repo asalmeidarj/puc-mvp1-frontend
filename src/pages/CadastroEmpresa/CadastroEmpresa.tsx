7822bf5a8970f3fa32d146d1b8eea091fed203ff
asalmeidarj@gmail.com <alessandro.almeida@soulcode.com>
2023-07-13 19:43:12 -0300

    refactor: remove interfaces and imports

--- src/pages/CadastroEmpresa/CadastroEmpresa.tsx
+++ src/pages/CadastroEmpresa/CadastroEmpresa.tsx
@@ -1,21 +1,6 @@
-import { Link, useParams, RedirectFunction, useSearchParams } from 'react-router-dom'
-
 import styles from './cadastro_empresa.module.css'
 import FormEmpresa from '../../components/FormEmpresa/FormEmpresa';
 
-interface IEmpresa {
-    id: number;
-    nome: string;
-    cnpj: string;
-    descricao: string;
-    logradouro: string;
-    numero: string;
-    complemento: string;
-    bairro: string;
-    cidade: string;
-    estado: string;
-    data_insercao: string;
-}
 
 export default function CadastroEmpresa() {
 
